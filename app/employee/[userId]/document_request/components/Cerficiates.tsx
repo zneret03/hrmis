07ff7e728404d4a4fa33ffbde832532ff9af34a4
ianDrilon03 <ian@findfoundation.org.au>
2025-09-29 15:29:34 +0800

    adding search title

--- app/employee/[userId]/document_request/components/Cerficiates.tsx
+++ app/employee/[userId]/document_request/components/Cerficiates.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { JSX } from 'react'
+import { JSX, useMemo } from 'react'
 import { PlusIcon } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { EmptyPlaceholder } from '@/components/custom/EmptyPlaceholder'
@@ -8,7 +8,10 @@ import { Pagination } from '@/components/custom/Pagination'
 import { Pagination as PaginationTypes } from '@/lib/types/pagination'
 import { Certificates as CertificatesType } from '@/lib/types/certificates'
 import { useCertificates } from '@/services/certificates/state/use-certificate'
+import { useRouter, usePathname } from 'next/navigation'
+import { Input } from '@/components/ui/input'
 import { CertificateCard } from './CertificateCard'
+import { debounce } from 'lodash'
 import { useShallow } from 'zustand/shallow'
 
 interface Certificate extends PaginationTypes {
@@ -25,9 +28,35 @@ export function Certificates({
     useShallow((state) => ({ toggleDialog: state.toggleOpenDialog }))
   )
 
+  const router = useRouter()
+  const pathname = usePathname()
+
+  const onDebounce = useMemo(
+    () =>
+      debounce((value) => {
+        if (!!value) {
+          router.replace(`${pathname}?page=${currentPage}&search=${value}`)
+          return
+        }
+
+        router.replace(`${pathname}?page=${currentPage}`)
+      }, 500),
+    [pathname, router, currentPage]
+  )
+
+  const onSearch = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const { value } = event.target
+    onDebounce(value)
+  }
+
   return (
-    <main>
-      <div className='text-right'>
+    <main className='space-y-8'>
+      <div className='flex items-center'>
+        <Input
+          placeholder='Search document by title...'
+          onChange={(event) => onSearch(event)}
+          className='max-w-sm'
+        />
         <Button onClick={() => toggleDialog?.(true, 'add', null)}>
           <PlusIcon /> Request Document
         </Button>
