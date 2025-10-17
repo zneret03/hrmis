d0427ed955865e924da097561c1a85875a9dc90b
Ian Drilon <ian@findfoundation.org.au>
2025-10-17 15:58:30 +0800

    updating prettierrc

--- app/backend/[userId]/requested_documents/components/DisapprovedRequestDialog.tsx
+++ app/backend/[userId]/requested_documents/components/DisapprovedRequestDialog.tsx
@@ -1,49 +1,49 @@
-'use client'
+'use client';
 
-import { JSX, useTransition } from 'react'
-import { DialogAlert } from '@/components/custom/DialogAlert'
-import { useRouter } from 'next/navigation'
-import { useShallow } from 'zustand/shallow'
-import { updateDocument } from '@/services/certificates/certificates.service'
-import { useCertificates } from '@/services/certificates/state/use-certificate'
+import { JSX, useTransition } from 'react';
+import { DialogAlert } from '@/components/custom/DialogAlert';
+import { useRouter } from 'next/navigation';
+import { useShallow } from 'zustand/shallow';
+import { updateDocument } from '@/services/certificates/certificates.service';
+import { useCertificates } from '@/services/certificates/state/use-certificate';
 
 export function DisapproveRequestDialog(): JSX.Element {
-  const [isPending, startTransition] = useTransition()
+  const [isPending, startTransition] = useTransition();
   const { open, type, toggleOpen, data } = useCertificates(
     useShallow((state) => ({
       open: state.open,
       type: state.type,
       data: state.data,
-      toggleOpen: state.toggleOpenDialog
-    }))
-  )
+      toggleOpen: state.toggleOpenDialog,
+    })),
+  );
 
-  const router = useRouter()
+  const router = useRouter();
 
   const resetVariables = (): void => {
-    toggleOpen?.(false, null, null, null)
-    router.refresh()
-  }
+    toggleOpen?.(false, null, null, null);
+    router.refresh();
+  };
 
   const onDisapproveLeaveRequest = async (): Promise<void> => {
     startTransition(async () => {
       await updateDocument(
         { certificate_status: 'disapproved' },
-        data?.id as string
-      )
-      resetVariables()
-    })
-  }
+        data?.id as string,
+      );
+      resetVariables();
+    });
+  };
 
   return (
     <DialogAlert
       open={open && type === 'disapprove'}
       title={`Disapproved Certificate Request?`}
-      description='Would you like to disapproved this request?'
+      description="Would you like to disapproved this request?"
       callback={onDisapproveLeaveRequest}
       cancel={() => toggleOpen?.(false, null, null, null)}
       isLoading={isPending}
-      type='error'
+      type="error"
     />
-  )
+  );
 }
