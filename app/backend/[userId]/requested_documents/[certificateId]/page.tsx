fb9b89c32bc6f6abcebe8669ed0619310dca4126
Ian Drilon <ian@findfoundation.org.au>
2025-10-24 15:35:30 +0800

    updating document editor and PDF editor

--- app/backend/[userId]/requested_documents/[certificateId]/page.tsx
+++ app/backend/[userId]/requested_documents/[certificateId]/page.tsx
@@ -1,5 +1,6 @@
 import { JSX } from 'react';
 import { PdfEditorPage } from '../components/editor/EditorPage';
+import { EditorSwitch } from '../components/editor/EditorSwitch';
 import { DocumentEditor } from './components/DocumentEditor';
 import { getTemplates } from '@/services/template/template.service';
 import { UploadExistingDialog } from '../components/editor/UploadExistingDialog';
@@ -10,9 +11,9 @@ export default async function EditorPage({
   searchParams,
 }: {
   params: Promise<{ certificateId: string }>;
-  searchParams: Promise<{ page: string }>;
+  searchParams: Promise<{ page: string; document: string }>;
 }): Promise<JSX.Element> {
-  const { page } = await searchParams;
+  const { page, document } = await searchParams;
   const { certificateId } = await params;
 
   const response = await getTemplates(
@@ -24,8 +25,14 @@ export default async function EditorPage({
       title="Document Editor"
       description="You can edit a document here"
     >
-      <PdfEditorPage {...{ templates: response.templates }} />
-      {/* <DocumentEditor certificateId={certificateId} /> */}
+      <section className="mb-4">
+        <EditorSwitch document={document} />
+      </section>
+      {document === 'pdf-editor' ? (
+        <PdfEditorPage {...{ templates: response.templates }} />
+      ) : (
+        <DocumentEditor certificateId={certificateId} />
+      )}
 
       <UploadExistingDialog />
     </Container>
---
+++ app/backend/[userId]/requested_documents/components/editor/EditorSwitch.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { JSX } from 'react';
+import { Switch } from '@/components/ui/switch';
+import { usePathname, useRouter } from 'next/navigation';
+
+export function EditorSwitch({ document }: { document: string }): JSX.Element {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleSwitchEditor = (e: boolean): void => {
+    const isDocumentEditor = e ? 'document-editor' : 'pdf-editor';
+    router.replace(`${pathname}?document=${isDocumentEditor}`);
+  };
+
+  const pdfEditor =
+    document === 'pdf-editor' ? 'PDF Editor' : 'Document Editor';
+
+  return (
+    <div className="flex items-center justify-end gap-2">
+      <Switch value={document} onCheckedChange={(e) => handleSwitchEditor(e)} />
+      <span className="font-semibold">{pdfEditor}</span>
+    </div>
+  );
+}
