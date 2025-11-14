ae3aa50095c7c00ebbea810f8de1536e3822ef5d
Ian Drilon <ian@findfoundation.org.au>
2025-11-14 08:54:22 +0800

    adding document templates page

---
+++ app/backend/[userId]/document_templates/page.tsx
@@ -0,0 +1,12 @@
+import { Container } from '@/components/custom/Container';
+
+export default async function LeaveCategories() {
+  return (
+    <Container
+      title="Document Templates"
+      description="You can manage your document templates here"
+    >
+      Document templates
+    </Container>
+  );
+}
